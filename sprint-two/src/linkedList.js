bbe5864ef3a184b02a22e99b8d0616cbc96b0812
MKS-Student <mks.student@hackreactor.com>
2015-03-05 16:32:58 -0800

    linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,45 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+
+    if (!list.head){
+      list.head = newNode;
+    } else {
+      list.tail.next = newNode;
+    }
+
+    list.tail = newNode;
   };
 
   list.removeHead = function(){
+    var newhead = list.head.next
+    var formerhead = list.head
+    delete list.head
+    list.head = newhead
+    return formerhead.value
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    var found = false;
+
+    while (currentNode.next !== null){
+      if (currentNode.value === target) {
+        found = true;
+        return found;
+      } 
+      currentNode = currentNode.next
+    } 
+
+    if (currentNode.next === null) {
+      if (currentNode.value === target) {
+        found = true;
+        return found;
+      } 
+    }
+
+    return found;
   };
 
   return list;
@@ -27,3 +60,5 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+list =
+{}
